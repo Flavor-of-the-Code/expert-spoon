f37a3f91efb2038bb37e4ea00a915241728d138e
Karzan Botani <karzan@botani.nu>
2022-06-09 16:41:19 +0200

    Basic func to calc xp needed for a lvl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::ops::Mul;
+
+fn needed_xp(level: u32) -> u32 {
+  let exponent = 1.5_f32;
+  let base_xp = 1000_f32;
+  let level_pow_exponent = (level as f32).powf(exponent);
+  base_xp.mul(level_pow_exponent) as u32
+}
+
 fn main() {
+  assert_eq!(needed_xp(1), 1000);
+  assert_eq!(needed_xp(2), 2828);
+  assert_eq!(needed_xp(3), 5196);
+
   println!("Hello, world!");
 }
